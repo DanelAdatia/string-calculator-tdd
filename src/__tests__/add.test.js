82890e09156d4dba81fdece5b3c2e78a13aa6867
Danel Adatia <daneladatia@gmail.com>
2024-11-07 16:53:29 +0530

    Add tests for sum of numbers with commas and newlines separators

--- src/__tests__/add.test.js
+++ src/__tests__/add.test.js
@@ -12,3 +12,7 @@ test('should return the same number for a single number digit',()=>{
 test('should return sum of the numbers separated by commas', () => {
   expect(add('1,2,3')).toBe(6);
 });
+
+test('should return sum of numbers separated by commas and newLines', ()=>{
+    expect(add('1\n2,3')).toBe(6);
+})
--- src/utils/add.js
+++ src/utils/add.js
@@ -1,6 +1,6 @@
 export function add(input){
   if (input === '') return 0;
 
-  const numbers = input.split(",").map((num)=>parseInt(num, 10))
+  const numbers = input.split(/,|\n/).map((num) => parseInt(num, 10));
   return numbers.reduce((acc, num) => acc + num, 0);
 }
